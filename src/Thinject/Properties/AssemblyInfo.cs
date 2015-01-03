1db870c8240d70e97b762224b7b3194de8698064
Kristian Hellang <kristian@hellang.com>
2015-01-03 05:30:01 +0100

    Added nuspec

--- src/Thinject/Properties/AssemblyInfo.cs
+++ src/Thinject/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
